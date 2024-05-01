40e5d9f72f5e51eedbea382e0580738212bd393a
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-01 12:50:01 +1000

    feat: add sidebar-section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,24 @@
 import styles from './App.module.css';
+import { motion } from 'framer-motion';
 
 const App = () => {
   return (
-    <main>
-      
+    <main className={styles.app}>
+      <motion.section className={styles.sidebar}
+        initial={{
+          width: '5rem',
+        }}
+        whileHover={{
+          width: '25vw'
+        }}
+      >
+        
+      </motion.section>
+      <article className={styles.canvas}>
+        <div className={styles.content}>
+
+        </div>
+      </article>
     </main>
   )
 }
