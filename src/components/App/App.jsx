34ffeb08bebbf34c1bd112e97c31ff1efc877cec
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-01 13:03:10 +1000

    fix: fix sidebar opacity issue

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,40 @@
 import styles from './App.module.css';
 import { motion } from 'framer-motion';
+import Sidebar from './Sidebar/Sidebar';
+import { useState } from 'react';
+
+const sidebar = {
+  inactive: {
+    width: '5rem'
+  },
+  active: {
+    width: '25rem'
+  }
+}
+const child = {
+  inactive: {
+    opacity: 0
+  },
+  active: {
+    opacity: 1
+  }
+}
 
 const App = () => {
+
   return (
     <main className={styles.app}>
-      <motion.section className={styles.sidebar}
-        initial={{
-          width: '5rem',
-        }}
-        whileHover={{
-          width: '25vw'
-        }}
+      <motion.section 
+        className={styles.sidebar}
+        variants={sidebar}
+        initial='inactive'
+        whileHover='active'
       >
-        
+        <motion.div
+          variants={child}
+        >
+          <Sidebar />
+        </motion.div>
       </motion.section>
       <article className={styles.canvas}>
         <div className={styles.content}>
---
+++ src/components/App/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import styles from './Sidebar.module.css';
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      hello
+    </div>
+  )
+}
+
+export default Sidebar
