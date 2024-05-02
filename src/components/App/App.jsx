23d63ce5baa1fe4f27d22c2bc6fb77d4b06f4fb1
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-02 12:36:55 +1000

    feat: add sidebar-slider

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,112 +1,54 @@
-import styles from './App.module.css';
-import { motion } from 'framer-motion';
-// import Sidebar from '../Sidebar/Sidebar';
-import SidebarHeader from '../SidebarHeader/SidebarHeader';
 import { useState } from 'react';
-import PersonalInput from '../PersonalInput/PersonalInput';
-import AboutInput from '../AboutInput/AboutInput';
-import ContactInput from '../ContactInput/ContactInput';
-import SkillsInput from '../SkillsInput/SkillsInput';
-
-const sidebar = {
-  inactive: {
-    width: '5rem'
-  },
-  active: {
-    width: '25rem'
-  }
-}
-const child = {
-  inactive: {
-    opacity: 0
-  },
-  active: {
-    opacity: 1
-  }
-}
+import { motion, AnimatePresence } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretRight, faCaretLeft } from '@fortawesome/free-solid-svg-icons';
+import styles from './App.module.css';
 
 const App = () => {
-  const [firstName, setFirstName] = useState(null);
-  const [lastName, setLastName] = useState(null);
-  const [middleName, setMiddleName] = useState(null);
-  const [position, setPosition] = useState(null);
-  const [bio, setBio] = useState(null);
-  const [contactNumber, setContactNumber] = useState(null);
-  const [emailAddress, setEmailAddress] = useState(null);
-  const [personalSite, setPersonalSite] = useState(null);
-  const [streetAddress, setStreetAddress] = useState(null);
-  const [skills, setSkills] = useState(['test', 'something else']);
-
-  const addSkill = (input) => {
-    const newSkill = input
-    setSkills(skills => [...skills, newSkill]);
-  }
-
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
     <main className={styles.app}>
-      <motion.section 
-        className={styles.sidebar}
-        variants={sidebar}
-        initial='inactive'
-        whileHover='active'
-        transition={{
-          delayChildren: 0.2
-        }}
-      >
-        <motion.div
-          variants={child}
+      <AnimatePresence>
+        {
+          showSidebar &&
+          <motion.div 
+            className={styles.sidebar_content}
+            initial={{
+              width: 0
+            }}
+            animate={{
+              width: '20vw'
+            }}
+            exit={{
+              width: 0
+            }}
+          >
+            
+          </motion.div>
+        }
+      </AnimatePresence>
+      <div className={styles.sidebar}>
+        <motion.button 
+          className={styles.sidebar_button}
+          onClick={() => setShowSidebar(!showSidebar)}
+          whileHover={{
+            scale: 1.1
+          }}
+          whileTap={{
+            scale: 0.9
+          }}
         >
-          <SidebarHeader />
-          <PersonalInput 
-            handleFirstName={(e) => setFirstName(e.target.value)}
-            handleLastName={(e) => setLastName(e.target.value)}
-            handleMiddleName={(e) => setMiddleName(e.target.value)}
-          />
-          <ContactInput 
-            handleContactNumber={(e) => setContactNumber(e.target.value)}
-            handleEmailAddress={(e) => setEmailAddress(e.target.value)}
-            handlePersonalSite={(e) => setPersonalSite(e.target.value)}
-            handleStreetAddress={(e) => setStreetAddress(e.target.value)}
-          />
-          <AboutInput 
-            handlePosition={(e) => setPosition(e.target.value)}
-            handleBio={(e) => setBio(e.target.value)}
-          />
-          <SkillsInput 
-            handleClick={(newSkill) => setSkills(skills => [...skills, newSkill])}
-          />
-        </motion.div>
-      </motion.section>
-      <article className={styles.canvas}>
-        <div className={styles.content}>
-          {firstName + ' ' + middleName + ' ' + lastName}
-          <div>
-            {position}
-          </div>
-          <div>
-            {bio}
-          </div>
-          <div>
-            {contactNumber}
-          </div>
-          <div>
-            {emailAddress}
-          </div>
-          <div>
-            {personalSite}
-          </div>
-          <div>
-            {streetAddress}
-          </div>
-          <ul>
-            {
-              skills.map((skill, index) =>
-              <li key={index}>{skill}</li>
-            )
-            }
-          </ul>
-        </div>
-      </article>
+          {
+            showSidebar ? 
+            <FontAwesomeIcon icon={faCaretLeft} />
+            :
+            <FontAwesomeIcon icon={faCaretRight} />
+          }
+        </motion.button>
+      </div>
+      <div className={styles.content}>
+        Content
+      </div>
     </main>
   )
 }
